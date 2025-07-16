fe4205fca84bd881e86f656ca05b54af48efe3e8
thetanav <tanavposwal939@gmail.com>
2025-07-16 20:30:52 +0530

    auth

---
+++ src/components/sign-up.tsx
@@ -0,0 +1,52 @@
+import { signIn } from "@/server/auth";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function SignUp() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Authenticate</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Sign in to Teamup</DialogTitle>
+          <DialogDescription>
+            Enter your email address to receive a magic link.
+          </DialogDescription>
+        </DialogHeader>
+        <form
+          action={async (formData) => {
+            "use server";
+            await signIn("resend", formData);
+          }}
+          className="flex gap-3"
+        >
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+            className="w-full"
+          />
+          <Button type="submit" variant={"ghost"}>
+            Sign In
+          </Button>
+        </form>
+        <DialogDescription>
+          The magic link will be sent to your email address.
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import Resend from "next-auth/providers/resend";
 
 import { db } from "@/server/db";
 import {
@@ -38,7 +38,9 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    Resend({
+      from: "no-reply@resend.dev",
+    }),
     /**
      * ...add more providers here.
      *
